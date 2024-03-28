8fc30b8e68a942d1f31a2941c2c1e9b62d16c161
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-29 01:10:48 +0700

    query relation

---
+++ src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package programmerzamannow.springdata.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import programmerzamannow.springdata.jpa.entity.Product;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByCategory_Name(String name);
+
+}
+
