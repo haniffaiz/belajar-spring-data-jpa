fe3a53b8a5eb4bc392ec515815c7345ae718bf04
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-29 18:49:47 +0700

    specification

--- src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
+++ src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import programmerzamannow.springdata.jpa.entity.Category;
@@ -18,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Product> findFirstByIdEquals(Long id);
